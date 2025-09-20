012a7974228097c45db256e8a7b0fa29dcb6710a
psgnezdilov <psoshenko8@gmail.com>
2025-09-20 20:07:28 +0100

    init.lua update

--- init.lua
+++ init.lua
@@ -882,20 +882,23 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'thesimonho/kanagawa-paper.nvim',
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {},
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      require('kanagawa-paper').setup {
+        integrations = {
+          wezterm = {
+            enabled = true,
+            path = (os.getenv 'TEMP' or '/tmp') .. '/nvim-theme',
+          },
         },
       }
-
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'kanagawa-paper'
     end,
   },
 
